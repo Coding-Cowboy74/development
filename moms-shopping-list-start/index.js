fb297128e0cb4448558f12adca2f676f164cb531
Steven Martin <sjmartin271974@gmail.com>
2023-06-25 08:07:50 -0500

    updating of the js file with delete commands and starter code for the adding. Added in a message feature too.

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -2,32 +2,82 @@
 
 Coding in here that once the user is to click the X button on that item section, it will remove the whole list item on that unordered list. */
 
+
+const unorderedList = document.getElementById("ul");
+
+const addingItems = document.getElementById("addItems");
+
+
+addingItems.addEventListener("click", function (e) {
+
+    let newListItem = document.createElement("li");
+    let addedItem = document.getElementById("title").value;
+
+    newListItem.textContent = addedItem;
+    unorderedList.appendChild(newListItem);
+
+    e.preventDefault();
+})
+
+/*    In the following section is the area of the deleting items. This will contain nothing more then the actions of the deleting functions. */
+
+/*  In the following code we are removing particular items from the shopping list by the click of the X button per line. This is being done by declaring the items li statement and the button itself. 
+
+Coding in here that once the user is to click the X button on that item section, it will remove the whole list item on that unordered list. */
+
 var coffee = document.getElementById("coffeePods");
 var coffeeButton = document.getElementById("coffeeDelete");
 
-coffeeButton.addEventListener("click", F => coffee.remove())
+coffeeButton.addEventListener("click", function () {
+    coffee.remove();
+    successMessage("coffee pods");
+})
 
 var myCereal = document.getElementById("cereal"); 
 var cerealButton = document.getElementById("btnDelCereal");
 
-cerealButton.addEventListener("click", F => myCereal.remove())
+cerealButton.addEventListener("click", function () {
+    myCereal.remove();
+    successMessage("cereal");
+})
 
 var mySyrup = document.getElementById("syrup");
 var syrupButton = document.getElementById("btnDelSyrup");
 
-syrupButton.addEventListener("click", F => mySyrup.remove())
+syrupButton.addEventListener("click", function() {
+    mySyrup.remove();
+    successMessage("maple syrup");
+})
 
 var myTacquitos = document.getElementById("taquitos");
 var taquButton = document.getElementById("btnDelTaq");
 
-taquButton.addEventListener("click", F => myTacquitos.remove())
+taquButton.addEventListener("click", function () {
+    myTacquitos.remove();
+    successMessage("taquitos");
+})
 
 var myBrats = document.getElementById("brats");
 var bratsButton = document.getElementById("btnDelBrats");
 
-bratsButton.addEventListener("click", F => myBrats.remove())
+bratsButton.addEventListener("click", function () {
+    myBrats.remove();
+    successMessage("brats");
+})
+
 
 var myPRolls = document.getElementById("pizzaRolls");
 var pRollsDelButton = document.getElementById("btnDelPRolls");
 
-pRollsDelButton.addEventListener("click", F => myPRolls.remove())
+pRollsDelButton.addEventListener("click", function () {
+    myBrats.remove();
+    successMessage("pizza rolls");
+})
+
+
+
+//    The following code is just a simple message box with a single parameter that is displaying a message box with a simple success statement with the included parameter to it
+
+function successMessage(message) {
+    window.alert("You have successfully removed " + message);
+}
