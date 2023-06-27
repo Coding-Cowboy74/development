86dafec3826002d63c2285fbc61f2f6b2aea489b
Steven Martin <sjmartin271974@gmail.com>
2023-06-27 14:59:41 -0500

    Updated the JS file to add new items with less code. It adds the text of item and the buttons all in one click.

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -3,21 +3,32 @@
 Coding in here that once the user is to click the X button on that item section, it will remove the whole list item on that unordered list. */
 
 
-const unorderedList = document.getElementById("ul");
+const unorderedList = document.getElementById("list");
 
 const addingItems = document.getElementById("addItems");
 
 
 addingItems.addEventListener("click", function (e) {
-
     let newListItem = document.createElement("li");
     let addedItem = document.getElementById("title").value;
 
-    newListItem.textContent = addedItem;
-    unorderedList.appendChild(newListItem);
+    //newListItem.textContent = addedItem;
+
+    let newDivText = document.createElement("div");
+    newDivText.innerHTML = document.getElementById("title").value;
+
+    let editButton = document.createElement("button");
+    let delButton = document.createElement("button");
+
+    editButton.textContent = "edit";
+    delButton.textContent = "X";
+
+
+    unorderedList.append(newListItem, newDivText, editButton, delButton);
 
     e.preventDefault();
 })
+
 
 /*    In the following section is the area of the deleting items. This will contain nothing more then the actions of the deleting functions. */
 
@@ -72,6 +83,23 @@ var pRollsDelButton = document.getElementById("btnDelPRolls");
 pRollsDelButton.addEventListener("click", function () {
     myBrats.remove();
     successMessage("pizza rolls");
+})
+
+
+     let podsEdit = document.getElementById("editPods");
+
+podsEdit.addEventListener("click", function () {
+
+    
+    let userInput = prompt("Please edit your entry!");
+
+    if (userInput === null || userInput === "") {
+        window.alert("You must enter the item name first!  Thanks!")
+        
+    }
+    else {
+        document.getElementById("liCoffee").innerHTML = userInput
+    }
 })
 
 
