6477f5781aecae50549127539bc802e87534d0de
Steven Martin <sjmartin271974@gmail.com>
2023-06-30 11:24:45 -0500

    updaes to the JS file, almost ccompleted edit button code; the delete code is still being edited though

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -9,20 +9,50 @@ const addingItems = document.getElementById("addItems");
 
 
 addingItems.addEventListener("click", function (e) {
-    let newListItem = document.createElement("li");
-    let addedItem = document.getElementById("title").value;
 
-    let newDivText = document.createElement("div");
-    newDivText.innerHTML = document.getElementById("title").value;
+    let newListItem = document.createElement("li");
+
+    let myDivText = document.createElement("div");
+    myDivText.innerHTML = document.getElementById("title").value;
 
     let editButton = document.createElement("button");
-    let delButton = document.createElement("button");
-
     editButton.textContent = "edit";
-    delButton.textContent = "X";
 
+    //Below here is the styling portion of the button element 
+    editButton.style.width = "43px";
+    editButton.style.height = "17";
+    editButton.style.marginLeft = "5em";
 
-    unorderedList.append(newListItem, newDivText, editButton, delButton);
+    console.log(e);     // logging the result
+
+    editButton.addEventListener("click", function(e) {
+        // Here we are just assigning the value of the prompt box that the user edits the value of to equal the new item name or value
+        let userInput = prompt("Please edit your entry.");
+
+        myDivText.innerHTML = userInput;
+
+        console.log(e);     // logging the result
+    })
+
+    let delButton = document.createElement("button");
+    delButton.textContent = "X"
+
+    //  Here in the following is the styling of the newly created button
+    delButton.style.width = "30px";
+    delButton.style.height = "17px";
+
+    delButton.addEventListener("click", function(e) {
+        newListItem.remove();
+
+        
+        console.log(e);   // logging the result 
+    })
+
+    //  Here in the following is the area of appending the elements to the DOM in the HTML page. All that is happening is that we are adding the main element the LI or list item, then the appendChild ones are where we are appending to the list item. 
+    unorderedList.append(newListItem);
+    unorderedList.appendChild(myDivText);
+    unorderedList.appendChild(editButton);
+    unorderedList.appendChild(delButton);
 
     e.preventDefault();
 })
@@ -34,44 +64,13 @@ addingItems.addEventListener("click", function (e) {
 
 Coding in here that once the user is to click the X button on that item section, it will remove the whole list item on that unordered list. */
 
-let buttonDelete = document.querySelectorAll(".btnDelete");
+let buttonDelete = document.querySelector(".btnDelete");
 
 let iList = document.querySelector("li");
 
 buttonDelete.addEventListener("click", function() {
-    for (i = 0; i < buttonDelete.length; i++) {
-        buttonDelete(i).remove();
-    }
+   iList.remove();
 })
-
-
-
-
-var myPRolls = document.getElementById("pizzaRolls");
-var pRollsDelButton = document.getElementById("btnDelPRolls");
-
-pRollsDelButton.addEventListener("click", function () {
-    myBrats.remove();
-    successMessage("pizza rolls");
-})
-
-
-     let podsEdit = document.getElementById("editPods");
-
-podsEdit.addEventListener("click", function () {
-
-    
-    let userInput = prompt("Please edit your entry!");
-
-    if (userInput === null || userInput === "") {
-        window.alert("You must enter the item name first!  Thanks!")
-        
-    }
-    else {
-        document.getElementById("liCoffee").innerHTML = userInput
-    }
-})
-
 
 
 //    The following code is just a simple message box with a single parameter that is displaying a message box with a simple success statement with the included parameter to it
